9659897152f5ea55b05feb17b135e779483d1888
Lukas Willburger <lukas.willburger@fim-rc.de>
2022-01-18 16:20:18 +0100

    removed gitignore

---
+++ target/classes/com/bka/ssi/generator/controller/Webhook.kt
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Copyright 2022 Bundesrepublik Deutschland
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.bka.ssi.generator.controller
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/webhook")
+class Webhook {
+
+    var logger: Logger = LoggerFactory.getLogger(Webhook::class.java)
+
+    @GetMapping("/")
+    private fun callback() {
+        this.logger.info("Received webhook callback.")
+    }
+}
