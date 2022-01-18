2e97e3d1d925aea7b61fd75ca8b8e920600ac510
Lukas Willburger <lukas.willburger@fim-rc.de>
2022-01-18 16:20:18 +0100

    removed gitignore

---
+++ target/test-classes/com/bka/ssi/generator/GeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bka.ssi.generator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
