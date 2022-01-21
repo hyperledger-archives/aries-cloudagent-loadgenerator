ff1e798d7bd2a832d2e3154e392aa38b75b53868
PaulWen <wenzel.pgb@googlemail.com>
2022-01-21 11:41:58 +0100

    refactor(Logger): Move logger into their own package
    Signed-off-by: PaulWen <wenzel.pgb@googlemail.com>

--- src/main/kotlin/com/bka/ssi/generator/application/logger/AcaPyLogger.kt
+++ src/main/kotlin/com/bka/ssi/generator/application/logger/AcaPyLogger.kt
@@ -1,4 +1,22 @@
-package com.bka.ssi.generator.application
+/*
+ *
+ *  * Copyright 2022 Bundesrepublik Deutschland
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.bka.ssi.generator.application.logger
 
 import org.hyperledger.aries.api.connection.ConnectionRecord
 import org.hyperledger.aries.api.issue_credential_v1.V1CredentialExchange
