1cd3fe932aba950e9dc227a814ebaac94d73d72f
PaulWen <wenzel.pgb@googlemail.com>
2022-01-21 17:40:19 +0100

    feat(Wallet Logger): Log the size of the issuer/holder wallet database periodically

---
+++ src/main/kotlin/com/bka/ssi/generator/application/logger/WalletLogger.kt
@@ -0,0 +1,26 @@
+package com.bka.ssi.generator.application.logger
+
+import com.bka.ssi.generator.domain.services.IWallet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+
+@Component
+class WalletLogger(
+    var wallet: IWallet
+) {
+    var logger: Logger = LoggerFactory.getLogger(WalletLogger::class.java)
+
+    @Scheduled(fixedRate = 10000)
+    fun reportCurrentTime() {
+        logger.info(
+            "Current size of the issuer/verifier database in bytes: ${wallet.walletDatabaseSizeInBytes()}, time: ${
+                Instant.now().toEpochMilli()
+            }"
+        )
+    }
+
+}
