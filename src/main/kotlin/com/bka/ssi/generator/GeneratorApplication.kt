52148f11fc58f21463cba2fac76ba5838e85cf08
PaulWen <wenzel.pgb@googlemail.com>
2022-01-21 17:40:19 +0100

    feat(Wallet Logger): Log the size of the issuer/holder wallet database periodically

--- src/main/kotlin/com/bka/ssi/generator/GeneratorApplication.kt
+++ src/main/kotlin/com/bka/ssi/generator/GeneratorApplication.kt
@@ -23,8 +23,10 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class GeneratorApplication {
 
 	@Bean
---
+++ src/main/kotlin/com/bka/ssi/generator/infrastructure/wallet/AcaPyPostgresWallet.kt
@@ -0,0 +1,26 @@
+package com.bka.ssi.generator.infrastructure.wallet
+
+import com.bka.ssi.generator.domain.services.IWallet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+
+
+@Service
+class AcaPyPostgresWallet(
+    val jdbcTemplate: JdbcTemplate,
+    @Value("\${issuer-verifier.wallet-db-name}") val walletDbName: String
+) : IWallet {
+    var logger: Logger = LoggerFactory.getLogger(AcaPyPostgresWallet::class.java)
+
+    override fun walletDatabaseSizeInBytes(): Int? {
+        val size = jdbcTemplate.queryForObject(
+            "SELECT PG_DATABASE_SIZE('$walletDbName')",
+            Int::class.java
+        )
+
+        return size
+    }
+}
