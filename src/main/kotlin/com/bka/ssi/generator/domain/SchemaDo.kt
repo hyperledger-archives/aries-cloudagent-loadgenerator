27895b77b633ec3cbeae2cee748a6324fd18c8d7
PaulWen <wenzel.pgb@googlemail.com>
2022-01-21 09:12:54 +0100

    refactor(Aries Client): Build a wrapper around the AcaPyAriesClient library to make it easier replaceable by a different AriesClient library
    1. to make it easier to time the HTTP requests the AriesClient
    2. to potentially be able to use this load generators to test other agents too

---
+++ src/main/kotlin/com/bka/ssi/generator/domain/SchemaDo.kt
@@ -0,0 +1,8 @@
+package com.bka.ssi.generator.domain
+
+class SchemaDo(
+    val attributes: List<String>,
+    val name: String,
+    val version: String
+) {
+}
---
+++ src/main/kotlin/com/bka/ssi/generator/infrastructure/ariesclient/AcaPyAriesClient.kt
@@ -0,0 +1,155 @@
+package com.bka.ssi.generator.infrastructure.ariesclient
+
+import com.bka.ssi.generator.domain.*
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import org.hyperledger.aries.AriesClient
+import org.hyperledger.aries.api.connection.CreateInvitationParams
+import org.hyperledger.aries.api.connection.CreateInvitationRequest
+import org.hyperledger.aries.api.connection.ReceiveInvitationRequest
+import org.hyperledger.aries.api.credential_definition.CredentialDefinition
+import org.hyperledger.aries.api.credentials.CredentialAttributes
+import org.hyperledger.aries.api.credentials.CredentialPreview
+import org.hyperledger.aries.api.issue_credential_v1.V1CredentialProposalRequest
+import org.hyperledger.aries.api.present_proof.PresentProofRequest
+import org.hyperledger.aries.api.schema.SchemaSendRequest
+
+
+class AcaPyAriesClient(
+    private val acaPy: AriesClient
+) : IAriesClient {
+
+    override fun getPublicDid(): String? {
+        val did = acaPy.walletDidPublic().orElse(null) ?: return null
+
+        return did.did
+    }
+
+    override fun createSchemaAndCredentialDefinition(schemaDo: SchemaDo): CredentialDefinitionDo {
+        val schemaSendResponse = acaPy.schemas(
+            SchemaSendRequest.builder()
+                .attributes(schemaDo.attributes)
+                .schemaName(schemaDo.name)
+                .schemaVersion(schemaDo.version)
+                .build()
+        )
+
+        if (schemaSendResponse.isEmpty) {
+            throw Exception("Failed to create schema.")
+        }
+
+        val credentialDefinitionResponse = acaPy.credentialDefinitionsCreate(
+            CredentialDefinition.CredentialDefinitionRequest.builder()
+                .schemaId(schemaSendResponse.get().schemaId)
+                .revocationRegistrySize(500)
+                .supportRevocation(true)
+                .tag("1.0")
+                .build()
+        )
+
+        if (credentialDefinitionResponse.isEmpty) {
+            throw Exception("Failed to create credential definition.")
+        }
+
+        return CredentialDefinitionDo(credentialDefinitionResponse.get().credentialDefinitionId)
+    }
+
+    override fun createConnectionInvitation(alias: String): ConnectionInvitationDo {
+        val createInvitationRequest = acaPy.connectionsCreateInvitation(
+            CreateInvitationRequest.builder().build(),
+            CreateInvitationParams(
+                alias,
+                true,
+                false,
+                false
+            )
+        )
+
+        if (createInvitationRequest.isEmpty) {
+            throw Exception("Failed to create connection invitation.")
+        }
+
+        return ConnectionInvitationDo(
+            createInvitationRequest.get().invitation.atType,
+            createInvitationRequest.get().invitation.atId,
+            createInvitationRequest.get().invitation.recipientKeys,
+            createInvitationRequest.get().invitation.serviceEndpoint,
+            createInvitationRequest.get().invitation.label
+        )
+    }
+
+    override fun receiveConnectionInvitation(connectionInvitationDo: ConnectionInvitationDo) {
+        val connectionRecord = acaPy.connectionsReceiveInvitation(
+            ReceiveInvitationRequest.builder()
+                .type(connectionInvitationDo.type)
+                .id(connectionInvitationDo.id)
+                .recipientKeys(connectionInvitationDo.recipientKeys)
+                .serviceEndpoint(connectionInvitationDo.serviceEndpoint)
+                .label(connectionInvitationDo.label)
+                .build(),
+            null
+        )
+
+        if (connectionRecord.isEmpty) {
+            throw Exception("Failed to receive connection invitation.")
+        }
+    }
+
+    override fun issueCredential(credentialDo: CredentialDo) {
+        val credentialExchange = acaPy.issueCredentialSend(
+            V1CredentialProposalRequest(
+                true,
+                true,
+                "Credential Offer",
+                credentialDo.connectionId,
+                credentialDo.credentialDefinitionId,
+                CredentialPreview(
+                    credentialDo.claims.map { CredentialAttributes(it.key, it.value) }
+                ),
+                null,
+                null,
+                null,
+                null,
+                null,
+                false
+            )
+        )
+
+        if (credentialExchange.isEmpty) {
+            throw Exception("Failed to issue credential.")
+        }
+    }
+
+    override fun sendProofRequest(proofRequestDo: ProofRequestDo) {
+        acaPy.presentProofSendRequest(
+            PresentProofRequest(
+                proofRequestDo.connectionId,
+                PresentProofRequest.ProofRequest.builder()
+                    .name("Proof Request")
+                    .nonRevoked(
+                        PresentProofRequest.ProofRequest.ProofNonRevoked(
+                            proofRequestDo.nonRevokedFrom,
+                            proofRequestDo.nonRevokedTo
+                        )
+                    )
+                    .requestedAttributes(
+                        proofRequestDo.requestedCredentials.mapIndexed { index: Int, credentialRequestDo: CredentialRequestDo ->
+                            "${index}_credential" to PresentProofRequest.ProofRequest.ProofRequestedAttributes.builder()
+                                .names(credentialRequestDo.claims)
+                                .restriction(
+                                    Gson().fromJson(
+                                        "{\"cred_def_id\": \"${credentialRequestDo.credentialDefinitionId}\"}",
+                                        JsonObject::class.java
+                                    )
+                                )
+                                .build()
+                        }.toMap()
+                    )
+                    .version("1.0")
+                    .build(),
+                false,
+                "Proof Request"
+            )
+        )
+    }
+}
