246c0b6af8802bc5077bec3994e257b19345670e
Lukas Willburger <lukas.willburger@fim-rc.de>
2022-01-27 10:27:26 +0100

    feat(logging): added Loki as log collector

--- src/main/kotlin/com/bka/ssi/generator/application/logger/WalletLogger.kt
+++ src/main/kotlin/com/bka/ssi/generator/application/logger/WalletLogger.kt
@@ -1,26 +1,22 @@
 package com.bka.ssi.generator.application.logger
 
 import com.bka.ssi.generator.domain.services.IWallet
+import java.time.Instant
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.Instant
-
 
 @Component
-class WalletLogger(
-    var wallet: IWallet
-) {
+class WalletLogger(var wallet: IWallet) {
     var logger: Logger = LoggerFactory.getLogger(WalletLogger::class.java)
 
     @Scheduled(fixedRate = 10000)
     fun reportCurrentTime() {
         logger.info(
-            "Current size of the issuer/verifier database in bytes: ${wallet.walletDatabaseSizeInBytes()}, time: ${
+                "type=database_size Current size of the issuer/verifier database in bytes: size=${wallet.walletDatabaseSizeInBytes()}, time=${
                 Instant.now().toEpochMilli()
             }"
         )
     }
-
 }
