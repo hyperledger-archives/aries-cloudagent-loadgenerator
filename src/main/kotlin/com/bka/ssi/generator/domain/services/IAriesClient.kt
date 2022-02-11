6943179c0577ec94cea326939680fe115a8b14d2
PaulWen <wenzel.pgb@googlemail.com>
2022-02-11 18:26:36 +0100

    feat(Revocable Credentials): Configure if a credential should be revocable, the size of the revocation registry, and if a proof request should include a non revoked check

--- src/main/kotlin/com/bka/ssi/generator/domain/services/IAriesClient.kt
+++ src/main/kotlin/com/bka/ssi/generator/domain/services/IAriesClient.kt
@@ -4,11 +4,16 @@ import com.bka.ssi.generator.domain.objects.*
 
 interface IAriesClient {
     fun getPublicDid(): String?
-    fun createSchemaAndCredentialDefinition(schemaDo: SchemaDo): CredentialDefinitionDo
+
+    fun createSchemaAndCredentialDefinition(
+        schemaDo: SchemaDo, revocable: Boolean,
+        revocationRegistrySize: Int
+    ): CredentialDefinitionDo
+
     fun createConnectionInvitation(alias: String): ConnectionInvitationDo
     fun receiveConnectionInvitation(connectionInvitationDo: ConnectionInvitationDo)
     fun issueCredentialToConnection(connectionId: String, credentialDo: CredentialDo)
-    fun sendProofRequestToConnection(connectionId: String, proofRequestDo: ProofRequestDo)
-    fun createOobProofRequest(proofRequestDo: ProofRequestDo): OobProofRequestDo
+    fun sendProofRequestToConnection(connectionId: String, proofRequestDo: ProofRequestDo, checkNonRevoked: Boolean)
+    fun createOobProofRequest(proofRequestDo: ProofRequestDo, checkNonRevoked: Boolean): OobProofRequestDo
     fun receiveOobProofRequest(oobProofRequestDo: OobProofRequestDo)
 }
