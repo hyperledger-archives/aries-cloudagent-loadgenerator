8c2e314673a0e864e9f6acd1680725a4a95cf7fd
PaulWen <wenzel.pgb@googlemail.com>
2022-02-11 20:45:37 +0100

    feat(Connection Flow): Test Flow that only established connections with the holder

---
+++ src/main/kotlin/com/bka/ssi/generator/application/testflows/ConnectionFlow.kt
@@ -0,0 +1,63 @@
+package com.bka.ssi.generator.application.testflows
+
+import com.bka.ssi.generator.application.testrunners.TestRunner
+import com.bka.ssi.generator.domain.objects.ConnectionRecordDo
+import com.bka.ssi.generator.domain.objects.CredentialExchangeRecordDo
+import com.bka.ssi.generator.domain.objects.ProofExchangeRecordDo
+import com.bka.ssi.generator.domain.services.IAriesClient
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+
+@Service
+@ConditionalOnProperty(
+    name = ["test-flows.connection-flow.active"],
+    matchIfMissing = false
+)
+class ConnectionFlow(
+    @Qualifier("IssuerVerifier") private val issuerVerifierAriesClient: IAriesClient,
+    @Qualifier("Holder") private val holderAriesClient: IAriesClient,
+) : TestFlow() {
+
+    protected companion object {
+        var testRunner: TestRunner? = null
+    }
+
+    override fun initialize(testRunner: TestRunner) {
+        logger.info("Initializing test flow...")
+        Companion.testRunner = testRunner
+
+        logger.info("Initialization completed.")
+    }
+
+    override fun startIteration() {
+        val connectionInvitation = issuerVerifierAriesClient.createConnectionInvitation("holder-acapy")
+
+        try {
+            holderAriesClient.receiveConnectionInvitation(connectionInvitation)
+        } catch (exception: Exception) {
+            logger.error("${exception.message} (Connection Invitation: ${connectionInvitation.toString()})")
+            testRunner?.finishedIteration()
+            return
+        }
+    }
+
+    override fun handleConnectionRecord(connectionRecord: ConnectionRecordDo) {
+        if (!connectionRecord.active) {
+            return
+        }
+
+        logger.info("Established new connection")
+
+        testRunner?.finishedIteration()
+    }
+
+    override fun handleCredentialExchangeRecord(credentialExchangeRecord: CredentialExchangeRecordDo) {
+        throw NotImplementedError("The connection flow does not handle credential exchange records.")
+    }
+
+    override fun handleProofRequestRecord(proofExchangeRecord: ProofExchangeRecordDo) {
+        throw NotImplementedError("The connection flow does not handle proof exchange records.")
+    }
+}
