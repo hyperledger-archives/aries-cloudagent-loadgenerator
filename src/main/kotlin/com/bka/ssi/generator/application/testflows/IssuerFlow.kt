37db8f7637b892e1f0d285ede028493270440b0e
PaulWen <wenzel.pgb@googlemail.com>
2022-02-11 17:31:43 +0100

    feat(Issuer Flow): Added a new test flow to simulate an issuer who is establishing connections to holders and issues credentials to them afterwards

---
+++ src/main/kotlin/com/bka/ssi/generator/application/testflows/IssuerFlow.kt
@@ -0,0 +1,89 @@
+package com.bka.ssi.generator.application.testflows
+
+import com.bka.ssi.generator.application.testrunners.TestRunner
+import com.bka.ssi.generator.domain.objects.*
+import com.bka.ssi.generator.domain.services.IAriesClient
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+
+@Service
+@ConditionalOnProperty(
+    name = ["test-flows.issuer-flow.active"],
+    matchIfMissing = false
+)
+class IssuerFlow(
+    @Qualifier("IssuerVerifier") private val issuerVerifierAriesClient: IAriesClient,
+    @Qualifier("Holder") private val holderAriesClient: IAriesClient,
+    @Value("\${test-flows.full-flow.use-connectionless-credential-issuance}") private val useConnectionlessCredentialIssuance: Boolean
+) : TestFlow() {
+
+    protected companion object {
+        var credentialDefinitionId = ""
+        var testRunner: TestRunner? = null
+    }
+
+    override fun initialize(testRunner: TestRunner) {
+        logger.info("Initializing test flow...")
+        Companion.testRunner = testRunner
+
+        val credentialDefinition = issuerVerifierAriesClient.createSchemaAndCredentialDefinition(
+            SchemaDo(
+                listOf("first name", "last name"),
+                "name",
+                "1.0"
+            )
+        )
+
+        credentialDefinitionId = credentialDefinition.id
+
+        logger.info("Initialization completed.")
+    }
+
+    override fun startIteration() {
+        val connectionInvitation = issuerVerifierAriesClient.createConnectionInvitation("holder-acapy")
+
+        try {
+            holderAriesClient.receiveConnectionInvitation(connectionInvitation)
+        } catch (exception: Exception) {
+            logger.error("${exception.message} (Connection Invitation: ${connectionInvitation.toString()})")
+            testRunner?.finishedIteration()
+            return
+        }
+    }
+
+    override fun handleConnectionRecord(connectionRecord: ConnectionRecordDo) {
+        if (!connectionRecord.active) {
+            return
+        }
+
+        logger.info("Established new connection")
+
+        issuerVerifierAriesClient.issueCredentialToConnection(
+            connectionRecord.connectionId,
+            CredentialDo(
+                credentialDefinitionId,
+                mapOf(
+                    "first name" to "Holder",
+                    "last name" to "Mustermann"
+                )
+            )
+        )
+
+    }
+
+    override fun handleCredentialExchangeRecord(credentialExchangeRecord: CredentialExchangeRecordDo) {
+        if (!credentialExchangeRecord.issued) {
+            return
+        }
+
+        logger.info("Issued credential")
+
+        testRunner?.finishedIteration()
+    }
+
+    override fun handleProofRequestRecord(proofExchangeRecord: ProofExchangeRecordDo) {
+    }
+}
