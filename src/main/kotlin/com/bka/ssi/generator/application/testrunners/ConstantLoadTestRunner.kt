0f0ee421607ffba98ce5f68e0e3d18415929dc8e
PaulWen <wenzel.pgb@googlemail.com>
2022-02-19 20:44:14 +0100

    fix(Constant and Increasing Load Runners): Previously, the scheduler used to generate the load was not running more than one task at a time. This caused the load generator not to generate the desired iterations per minute.

--- src/main/kotlin/com/bka/ssi/generator/application/testrunners/ConstantLoadTestRunner.kt
+++ src/main/kotlin/com/bka/ssi/generator/application/testrunners/ConstantLoadTestRunner.kt
@@ -5,6 +5,7 @@ import com.bka.ssi.generator.application.testflows.TestFlow
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
@@ -20,11 +21,12 @@ class ConstantLoadTestRunner(
     private val errorLogger: ErrorLogger,
     @Value("\${test-runners.constant-load-runner.number-of-total-iterations}") val numberOfTotalIterations: Int,
     @Value("\${test-runners.constant-load-runner.number-of-iterations-per-minute}") val numberOfIterationsPerMinute: Int,
-    @Value("\${test-runners.constant-load-runner.core-thread-pool-size}") val coreThreadPoolSize: Int
+    @Value("\${test-runners.constant-load-runner.thread-pool-size}") val threadPoolSize: Int
 ) : TestRunner(
 ) {
 
     lateinit var loadScheduler: ScheduledFuture<*>
+    lateinit var fixedThreadPoolExecutor: ExecutorService
 
     protected companion object {
         var numberOfIterationsStarted = 0
@@ -39,16 +41,16 @@ class ConstantLoadTestRunner(
         logger.info("Starting ConstantLoadTestRunner...")
         logger.info("Number of Iterations: $numberOfTotalIterations")
         logger.info("Number of Iterations per Minute: $numberOfIterationsPerMinute")
-        logger.info("Core thread pool size: $coreThreadPoolSize")
+        logger.info("Thread pool size: $threadPoolSize")
         logger.info("Expected running duration in minutes: ${numberOfTotalIterations / numberOfIterationsPerMinute}")
 
-        val executor = Executors.newScheduledThreadPool(coreThreadPoolSize)
+        fixedThreadPoolExecutor = Executors.newFixedThreadPool(threadPoolSize)
+
+        val executor = Executors.newScheduledThreadPool(4)
         loadScheduler = executor.scheduleAtFixedRate(
             Runnable {
                 try {
-                    numberOfIterationsStarted++
-                    logger.info("Started $numberOfIterationsStarted of $numberOfTotalIterations iterations")
-                    testFlow.startIteration()
+                    fixedThreadPoolExecutor.submit { startNewIteration() }
                 } catch (exception: Exception) {
                     errorLogger.reportTestRunnerError("The 'loadScheduler' of the 'ConstantLoadTestRunner' caught an error: ${exception.message} [${exception.printStackTrace()}]")
                     exception.printStackTrace();
@@ -60,12 +62,19 @@ class ConstantLoadTestRunner(
         )
     }
 
+    private fun startNewIteration() {
+        numberOfIterationsStarted++
+        logger.info("Started $numberOfIterationsStarted of $numberOfTotalIterations iterations")
+        testFlow.startIteration()
+    }
+
     override fun finishedIteration() {
         numberOfIterationsFinished++
         logger.info("Finished ${numberOfIterationsFinished} of $numberOfTotalIterations iterations")
 
         if (numberOfIterationsFinished >= numberOfTotalIterations) {
             loadScheduler.cancel(false)
+            fixedThreadPoolExecutor.shutdown()
         }
     }
 
