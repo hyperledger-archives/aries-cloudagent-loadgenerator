c254fa38387b7d4643fdc276934ab792ce6a64e5
iliecirciumaru <34098446+iliecirciumaru@users.noreply.github.com>
2022-03-15 11:18:20 +0100

    Implement monitoring of the Spring Boot app (#62)
    Author: Ilie Circiumaru <ici@zurich.ibm.com>
    Issue: /My-DIGI-ID/aries-cloudagent-loadgenerator/issues/45
    Reviewer Paul Wen

--- src/main/kotlin/com/bka/ssi/generator/config/SwaggerConfig.kt
+++ src/main/kotlin/com/bka/ssi/generator/config/SwaggerConfig.kt
@@ -1,7 +1,20 @@
 package com.bka.ssi.generator.config
 
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.util.StringUtils
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -10,6 +23,8 @@ import springfox.documentation.service.Tag
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+
 
 /**
  * Build a Swagger2 configuration file
@@ -40,5 +55,46 @@ class SwaggerConfig {
             .description(description)
             .version(version)
             .build()
+    }
+
+    // This bean resolves problem with actuator and swagger
+    // https://github.com/springfox/springfox/issues/3462#issuecomment-1010721223
+    @Bean
+    fun webEndpointServletHandlerMapping(
+        webEndpointsSupplier: WebEndpointsSupplier,
+        servletEndpointsSupplier: ServletEndpointsSupplier,
+        controllerEndpointsSupplier: ControllerEndpointsSupplier,
+        endpointMediaTypes: EndpointMediaTypes?,
+        corsProperties: CorsEndpointProperties,
+        webEndpointProperties: WebEndpointProperties,
+        environment: Environment
+    ): WebMvcEndpointHandlerMapping? {
+        val allEndpoints: MutableList<ExposableEndpoint<*>?> = ArrayList()
+        val webEndpoints = webEndpointsSupplier.endpoints
+        allEndpoints.addAll(webEndpoints)
+        allEndpoints.addAll(servletEndpointsSupplier.endpoints)
+        allEndpoints.addAll(controllerEndpointsSupplier.endpoints)
+        val basePath = webEndpointProperties.basePath
+        val endpointMapping = EndpointMapping(basePath)
+        val shouldRegisterLinksMapping = shouldRegisterLinksMapping(webEndpointProperties, environment, basePath)
+        return WebMvcEndpointHandlerMapping(
+            endpointMapping,
+            webEndpoints,
+            endpointMediaTypes,
+            corsProperties.toCorsConfiguration(),
+            EndpointLinksResolver(allEndpoints, basePath),
+            shouldRegisterLinksMapping,
+            null
+        )
+    }
+
+    private fun shouldRegisterLinksMapping(
+        webEndpointProperties: WebEndpointProperties,
+        environment: Environment,
+        basePath: String
+    ): Boolean {
+        return webEndpointProperties.discovery.isEnabled && (StringUtils.hasText(basePath) || ManagementPortType.get(
+            environment
+        ) == ManagementPortType.DIFFERENT)
     }
 }
