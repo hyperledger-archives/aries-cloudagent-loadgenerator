93714b963dd86a95122220630b146f5d0b52af1d
PaulWen <wenzel.pgb@googlemail.com>
2023-01-05 13:33:34 +0100

    feat(manage.sh): Wait for the ledger to start until the DID was registered successfully to the ledger

--- setup/manage.sh
+++ setup/manage.sh
@@ -121,13 +121,17 @@ function startDashboard() {
 function startIndyNetwork() {
   echo "Starting the VON Network ..."
   ./von-network/manage build
-  ./von-network/manage start
+  ./von-network/manage start --wait
 
-  echo "Waiting for the ledger to start... (sleeping 30 seconds)"
-  sleep 30
+  DID_REGISTRATION_RESULT="Not ready"
+  while [[ $DID_REGISTRATION_RESULT == *"Not ready"* ]]; do
+    echo "Waiting for the ledger to start... (sleeping 10 seconds)"
+    sleep 10
 
-  echo "Registering issuer DID..."
-  curl -d "{\"role\": \"ENDORSER\", \"seed\":\"$ISSUER_DID_SEED\"}" -H "Content-Type: application/json" -X POST $LEDGER_REGISTER_DID_ENDPOINT_HOST
+    echo "Registering issuer DID..."
+    DID_REGISTRATION_RESULT=`curl -d "{\"role\": \"ENDORSER\", \"seed\":\"$ISSUER_DID_SEED\"}" -H "Content-Type: application/json" -X POST $LEDGER_REGISTER_DID_ENDPOINT_HOST`
+    echo $DID_REGISTRATION_RESULT
+  done
 }
 
 function startAgents() {
